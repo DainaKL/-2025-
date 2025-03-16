605f6bf15ca57fc5a7fcf45f1dc7135e41a7e2ee
Дарья <dasasor2006@gmail.com>
2025-03-16 22:02:14 +0400

    Первый коммит
    Hello world

--- main.cpp
+++ main.cpp
@@ -1,20 +1,5 @@
 #include <iostream>
-#include <vector>
-using namespace std;
-long long fibonacci(int n, vector<long long>& fibok) {
-    if (n <= 0) return 0;
-    else if (n == 1) return 0;
-    else if (n == 2) return 1;
-    fibok[n] = fibonacci(n - 1, fibok) + fibonacci(n - 2, fibok);
-    return fibok[n];
-}
+unsing namespace std;
 int main(){
-    int n;
-    cin >> n;
-    vector<long long> fibok(n + 1, -1);
-    for (int i = 1; i <= n; i++) {
-    	long long result = fibonacci(i, fibok);
-    	cout << result<< endl;
-    }
-    return 0;
+   cout<< "Hello, World!";
 }
